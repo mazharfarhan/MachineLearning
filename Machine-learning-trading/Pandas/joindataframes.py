d67aa8200f33e6607332e8eea8d18de0ff66fb1e
Farhan Mazhar <fmazhar@usc.edu>
2017-02-25 23:20:32 -0800

    Create joindataframes.py

---
+++ Machine-learning-trading/Pandas/joindataframes.py
@@ -0,0 +1,39 @@
+"""Utility functions"""
+
+import os
+import pandas as pd
+
+def symbol_to_path(symbol, base_dir="data"):
+    """Return CSV file path given ticker symbol."""
+    return os.path.join(base_dir, "{}.csv".format(str(symbol)))
+
+
+def get_data(symbols, dates):
+    """Read stock data (adjusted close) for given symbols from CSV files."""
+    df = pd.DataFrame(index=dates)
+    if 'SPY' not in symbols:  # add SPY for reference, if absent
+        symbols.insert(0, 'SPY')
+
+    for symbol in symbols:
+        # TODO: Read and join data for each symbol
+        path = symbol_to_path(symbol)
+        df1 = pd.read_csv(path, index_col="Date",parse_dates=True,usecols = ['Date', 'Adj Close'], na_values = ['nan'])
+        df1 = df1.rename(columns = {"Adj Close" : symbol})
+        df = df.join(df1, how = "inner")
+    return df
+
+
+def test_run():
+    # Define a date range
+    dates = pd.date_range('2010-01-22', '2010-01-26')
+
+    # Choose stock symbols to read
+    symbols = ['GOOG', 'IBM', 'GLD']
+    
+    # Get stock data
+    df = get_data(symbols, dates)
+    print df
+
+
+if __name__ == "__main__":
+    test_run()
