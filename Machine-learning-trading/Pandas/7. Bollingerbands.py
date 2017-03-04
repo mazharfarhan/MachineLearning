c475a377707ca3da66cb99d5b8c19ef6a999ca26
Farhan Mazhar <fmazhar@usc.edu>
2017-03-03 20:57:10 -0800

    Create 7. Bollingerbands.py

---
+++ Machine-learning-trading/Pandas/7. Bollingerbands.py
@@ -0,0 +1,3 @@
+# how to know the standard devation is deviating significantly from the mean so as to gain attention from a trading point of view ?
+
+#sol: Bollinger band - if there is volatility in the stock, then we can discard the deviations towards the top and bottom of the mean. If #the deviations are 2 standard deviation away from the mean, then we should pay attention.
