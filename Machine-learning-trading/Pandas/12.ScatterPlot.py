a58f38246f7d9988d86d4262860b6d46c7d24b5a
Farhan Mazhar <fmazhar@usc.edu>
2017-03-05 23:58:44 -0800

    Update 12.ScatterPlot.py

--- Machine-learning-trading/Pandas/12.ScatterPlot.py
+++ Machine-learning-trading/Pandas/12.ScatterPlot.py
@@ -1,1 +1,11 @@
 
+def test_run2():
+    dates = pd.date_range('2009-01-01','2012-12-31')
+    
+    symbols = ['SPY', 'XOM']
+    df = get_data(symbols, dates)
+    plot_data(df)
+    
+    daily_returns = compute_daily_returns(df)
+    daily_returns.plot(kind='scatter')
+   
